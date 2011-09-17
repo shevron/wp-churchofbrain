4abb734be37ce007d11231a930ad38bb4aaa6c1d
Shahar Evron <shahar.evron@gmail.com>
2011-09-17 14:27:50 +0300

    Changing footer copyright and theme credits
    Change-Id: I70e10e33d71185f6466c569b691547620a390da1

--- footer.php
+++ footer.php
@@ -17,7 +17,10 @@
 			<?php do_action( 'toolbox_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'toolbox' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'toolbox' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'toolbox' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'toolbox' ), 'Toolbox', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			Theme: <a href="http://github.com/shevron/wp-churchofbrain">Church of Brain</a>, based on Toolbox by <a href="http://automattic.com/" rel="designer">Automattic</a>
+		</div>
+		<div id="copyright">
+		    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
